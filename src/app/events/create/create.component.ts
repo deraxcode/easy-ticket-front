7555c6fbdaf7742e16200415ffdab93de1bb713b
jorge flores <jorge.flores.s@taisachile.cl>
2018-08-01 17:53:39 -0400

    avance creacion de eventos

--- src/app/events/create/create.component.ts
+++ src/app/events/create/create.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { EventsService } from '../events.service';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 @Component({
-  selector: 'app-create',
+  selector: 'app-events-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  createEventFormStep1: FormGroup;
+  createEventFormStep2: FormGroup;
+  createEventFormStep3: FormGroup;
+  regions : any;
+  communes : any;
+
+  constructor(private eventsService : EventsService , private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.createEventFormStep1 = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: ['',Validators.required],
+      address: ['', Validators.email],
+      addressNumber: ['', Validators.required],
+      addressDescription: ['', Validators.required],
+      region: ['', Validators.required],
+      commune: ['', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required]
+    });
+
+    this.eventsService.findAllRegions().subscribe(response => {
+      this.regions = response.data;
+    });
+
+    this.communes = [];
+
   }
+
+  toStep2(){
+
+  }
+
+
 
 }
--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import {EventsRoutingModule} from './events-routing.module';
 import { CreateComponent } from './create/create.component';
+import { EventsService } from './events.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
-    EventsRoutingModule
+    EventsRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  declarations: [HomeComponent, CreateComponent]
+  declarations: [HomeComponent, CreateComponent],
+  providers: [EventsService]
 })
 export class EventsModule { }
